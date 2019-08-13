7926896aa1195db7ca6394c1ce60152b98f5fca1
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2019-08-13 07:41:39 +0200

    Fix issue #4 by removing the two
    add-to-list calls.  Update README as well.

--- helm-org.el
+++ helm-org.el
@@ -34,9 +34,6 @@
 
 (defvar helm-completing-read-handlers-alist)
 
-(add-to-list 'helm-completing-read-handlers-alist '(org-capture . helm-org-completing-read-tags))
-(add-to-list 'helm-completing-read-handlers-alist '(org-set-tags . helm-org-completing-read-tags))
-
 ;; Menu
 ;;;###autoload
 (easy-menu-add-item
